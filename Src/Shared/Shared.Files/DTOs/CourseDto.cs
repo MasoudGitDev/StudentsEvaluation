1b7019a330e34c17f0e59fff091a4e0cad6383b7
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 20:41:35 +0330

    Create : CourseDTO to handle income value related that by a validator later

---
+++ Src/Shared/Shared.Files/DTOs/CourseDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Files.DTOs;
+
+public record CourseDto(string Code , string Name, string TeacherPersonnelCode) {
+    public static CourseDto New(string code , string name , string teacherPersonnelCode)
+        => new(code,name,teacherPersonnelCode);
+}
