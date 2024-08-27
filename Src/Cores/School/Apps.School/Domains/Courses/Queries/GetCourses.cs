c1c7a29afc9dd86a70316950f432dc72f8d42e72
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 20:36:39 +0330

    Create : GetCourses query and its mediatR handler

---
+++ Src/Cores/School/Apps.School/Domains/Courses/Queries/GetCourses.cs
@@ -0,0 +1,18 @@
+﻿using Apps.School.Constants;
+using Domains.School.Abstractions;
+using Domains.School.Course.Aggregate;
+using MediatR;
+using Shared.Files.Models;
+
+namespace Apps.School.Domains.Courses.Queries;
+public sealed record GetCourses() : IRequest<Result<List<Course>>> {
+    public static GetCourses New() => new();
+}
+
+//====================handler
+internal sealed class GetCoursesHandler(ISchoolUOW _unitOfWork)
+    : SchoolRequestHandler<GetCourses , Result<List<Course>>>(_unitOfWork) {
+    public override async Task<Result<List<Course>>> Handle(GetCourses request , CancellationToken cancellationToken) {
+        return SuccessListResult(nameof(QueryPropertyNames.Courses) , await GetCoursesAsync());
+    }
+}
