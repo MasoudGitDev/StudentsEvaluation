25ce41a1c31a1ce72fc2375b36bdc3300d7e9288
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 11:06:03 +0330

    Create : Teacher DTO to use validaion later

---
+++ Src/Shared/Shared.Files/DTOs/TeacherDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Files.DTOs;
+
+public record TeacherDto(string FirstName , string LastName , string PersonnelCode) {
+    public static TeacherDto New(string firstName , string lastName , string personnelCode)
+        => new(firstName , lastName , personnelCode);
+}
