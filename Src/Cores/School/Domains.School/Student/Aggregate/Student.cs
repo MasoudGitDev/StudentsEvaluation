f714372997b69b4d11edfc62b429681355c720b4
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 06:55:45 +0330

    Update : Student inherit from Person Model

--- Src/Cores/School/Domains.School/Student/Aggregate/Student.cs
+++ Src/Cores/School/Domains.School/Student/Aggregate/Student.cs
@@ -1,10 +1,7 @@
-﻿namespace Domains.School.Student.Aggregate;
-public partial class Student {
-    public ulong Id { get; private set; }
-    public string FirstName { get; private set; } = null!;
-    public string LastName { get; private set; } = null!;
-    public string NationalCode { get; private set; } = null!;
+﻿using Domains.School.Shared;
 
-    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+namespace Domains.School.Student.Aggregate;
+public partial class Student : Person {
+    public string NationalCode { get; private set; } = null!;
 }
 
