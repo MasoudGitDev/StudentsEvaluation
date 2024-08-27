ab751768c5570749d93e7f5811cf5cc949db35c0
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 06:59:48 +0330

    Create : Basic Course Table model

---
+++ Src/Cores/School/Domains.School/Course/Aggregate/Course.cs
@@ -0,0 +1,6 @@
+﻿namespace Domains.School.Course.Aggregate;
+public partial class Course {
+    public ulong Id { get; private set; }
+    public ulong Code { get; private set; }
+    public string Name { get; private set; } = null!;
+}
---
+++ Src/Cores/School/Domains.School/Course/Aggregate/CourseCommands.cs
@@ -0,0 +1,8 @@
+﻿namespace Domains.School.Course.Aggregate;
+public partial class Course {
+    public static Course New(ulong code , string name)
+        => new() {
+             Code = code ,
+             Name = name
+        };
+}
