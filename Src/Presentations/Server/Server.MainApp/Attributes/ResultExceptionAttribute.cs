76e1e3b93b8dd251df71fa585c6ba45e0f9b313c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 20:39:12 +0330

    Create : ResultException attribute to handle exceptions

---
+++ Src/Presentations/Server/Server.MainApp/Attributes/ResultExceptionAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Shared.Files.Exceptions;
+using Shared.Files.Models;
+
+namespace Server.MainApp.Attributes;
+
+public class ResultExceptionAttribute : ExceptionFilterAttribute {
+    public override void OnException(ExceptionContext context) {
+        if(context.Exception is CustomException customException) {
+            context.Result = new JsonResult(Result.Failed(customException.Description));    
+        }
+        else {
+            context.Result = new JsonResult(Result.Failed(context.Exception.Message));
+        }
+    }
+}
