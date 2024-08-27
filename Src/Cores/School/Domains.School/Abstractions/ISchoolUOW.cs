ab798cef1d0a74e4a7578f86bf8ab1d3de5e410c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 08:21:46 +0330

    create : School Unit Of Work

---
+++ Src/Cores/School/Domains.School/Abstractions/ISchoolUOW.cs
@@ -0,0 +1,9 @@
+﻿namespace Domains.School.Abstractions {
+    public interface ISchoolUOW {
+        public ISchoolQueries Queries { get; }
+
+        public Task CreateAsync<TEntity>(TEntity entity);
+        public Task DeleteAsync<TEntity>(TEntity entity);
+        public Task SaveChangesAsync();
+    }
+}
