6a946fde8a29338d1e210f405d600d8404e8520b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 20:42:37 +0330

    Create : SchoolController to handle better those controllers that related to school

---
+++ Src/Presentations/Server/Server.MainApp/Controllers/SchoolController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.MainApp.Attributes;
+
+namespace Server.MainApp.Controllers;
+
+[ResultException]
+[ApiController]
+public class SchoolController : ControllerBase {
+
+}
