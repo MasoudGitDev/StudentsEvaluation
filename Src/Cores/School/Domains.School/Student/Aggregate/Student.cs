a61b854bebe89f3d5e75fec92fa869be6cac73ac
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 06:42:06 +0330

    Create : Basic Student Aggregate

---
+++ Src/Cores/School/Domains.School/Student/Aggregate/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Domains.School.Student.Aggregate;
+public partial class Student {
+    public ulong Id { get; private set; }
+    public string FirstName { get; private set; } = null!;
+    public string LastName { get; private set; } = null!;
+    public string NationalCode { get; private set; } = null!;
+
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+}
+
---
+++ Src/Cores/School/Domains.School/Student/Aggregate/StudentCommands.cs
@@ -0,0 +1,9 @@
+﻿namespace Domains.School.Student.Aggregate;
+public partial class Student {
+    public static Student New(string firstName , string lastName , string nationalCode)
+        => new() {
+            FirstName = firstName ,
+            LastName = lastName ,
+            NationalCode = nationalCode
+        };
+}
