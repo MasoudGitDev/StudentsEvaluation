72735208d22a32368b2486258a5a9f8ea3a6a462
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 08:21:06 +0330

    Create : Some basic Stutent Queries

---
+++ Src/Cores/School/Domains.School/Student/Repo/IStudentQueries.cs
@@ -0,0 +1,9 @@
+﻿using StudentTbl = Domains.School.Student.Aggregate.Student;
+
+namespace Domains.School.Student.Repo;
+
+public interface IStudentQueries {
+    Task<StudentTbl?> GetById(ulong id);
+    Task<StudentTbl?> GetByNationalCode(string nationalCode);
+    Task<List<StudentTbl>> GetAllAsync();
+}
