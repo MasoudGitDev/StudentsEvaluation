8d021d9bf85659268b4ce3c1ae48db7982335fee
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 10:15:59 +0330

    Create : Description value object ti handle description of exception better

---
+++ Src/Shared/Shared.Files/ValueObjects/Description.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Files.ValueObjects;
+public record Description(string Content) {
+
+    public static implicit operator string(Description description) => description.Content;
+    public static implicit operator Description(string content) => new(content);
+}
