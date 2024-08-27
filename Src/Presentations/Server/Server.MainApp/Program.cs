5cab11db54470cf1ae5f267faa064b346b20d689
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 07:48:18 +0330

    update : add AddInfraLayerServices

--- Src/Presentations/Server/Server.MainApp/Program.cs
+++ Src/Presentations/Server/Server.MainApp/Program.cs
@@ -1,6 +1,11 @@
+using Infra.SqlServerWithEF.Extensions; 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddInfraLayerServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
