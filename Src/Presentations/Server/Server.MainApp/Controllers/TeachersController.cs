e888a76cfab99bd469dfac1c3f861665fe644a98
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 11:06:46 +0330

    Create : basic actions (GetAll and create) for teacher in related api controller

---
+++ Src/Presentations/Server/Server.MainApp/Controllers/TeachersController.cs
@@ -0,0 +1,24 @@
+﻿using Apps.School.Domains.Teachers.Commands;
+using Apps.School.Domains.Teachers.Queries;
+using Domains.School.Teacher.Aggregate;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Files.DTOs;
+using Shared.Files.Models;
+
+namespace Server.MainApp.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TeachersController(IMediator _mediator) : ControllerBase {
+
+    [HttpGet("All")]
+    public async Task<Result<List<Teacher>>> GetAllAsync() {
+        return await _mediator.Send(GetTeachers.New());
+    }
+
+    [HttpPost("Create")]
+    public async Task<Result> CreateAsync([FromBody] TeacherDto model) {
+        return await _mediator.Send(model.Adapt<Create>());
+    }
+}
