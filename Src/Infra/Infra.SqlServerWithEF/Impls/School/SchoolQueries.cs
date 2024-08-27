99b82d0f36dd6c9140ef05b6a193b6da0ba815a2
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 10:14:10 +0330

    Creat : basic School Queries

---
+++ Src/Infra/Infra.SqlServerWithEF/Impls/School/SchoolQueries.cs
@@ -0,0 +1,9 @@
+﻿using Domains.School.Abstractions;
+using Domains.School.Student.Repo;
+
+namespace Infra.SqlServerWithEF.Impls.School;
+internal sealed class SchoolQueries(
+    IStudentQueries _studentQueries
+    ) : ISchoolQueries {
+    public IStudentQueries Students => _studentQueries;
+}
