0be1b2ea72f909cb9d241d3e7d7fc70f516723f5
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 10:16:45 +0330

    update : register medaitR

--- Src/Presentations/Server/Server.MainApp/Program.cs
+++ Src/Presentations/Server/Server.MainApp/Program.cs
@@ -1,3 +1,4 @@
+using Apps.School;
 using Infra.SqlServerWithEF.Extensions; 
 
 
@@ -6,6 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddInfraLayerServices();
+builder.Services.AddMediatR(config => {
+    config.RegisterServicesFromAssemblies(
+        typeof(AppsSchoolAssembly).Assembly
+    );
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
