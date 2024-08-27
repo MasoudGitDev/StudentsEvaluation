9d5ac9db49ce3f6f749b6fddd7e5df8e0acf8ac5
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 10:14:49 +0330

    create : basic student queries

---
+++ Src/Infra/Infra.SqlServerWithEF/Impls/School/StudentQueries.cs
@@ -0,0 +1,19 @@
+﻿using Domains.School.Student.Aggregate;
+using Domains.School.Student.Repo;
+using Infra.SqlServerWithEF.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.SqlServerWithEF.Impls.School;
+internal sealed class StudentQueries(AppDbContext _dbContext) : IStudentQueries {
+    public async Task<List<Student>> GetAllAsync() {
+        return await _dbContext.Students.ToListAsync();
+    }
+
+    public async Task<Student?> GetById(ulong id) {
+        return await _dbContext.Students.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Student?> GetByNationalCode(string nationalCode) {
+        return await _dbContext.Students.FirstOrDefaultAsync(x => x.NationalCode == nationalCode);
+    }
+}
