d7236b7d95b61e42b8b39ac51241777109056d15
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 08:03:42 +0330

    create : custom exception to handle exception

---
+++ Src/Shared/Shared.Files/Exceptions/CustomException.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Files.Exceptions;
+public class CustomException : Exception {
+    public string Code { get; private set; } = "<unknown-code>";
+    public string Description { get; private set; } = "<unknown-description";
+
+    public CustomException(string code , string description) {
+        Code = code;
+        Description = description;
+    }
+}
