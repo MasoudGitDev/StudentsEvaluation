2a4adda3b01a5142c7151ba3e92177e098a65661
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 08:04:53 +0330

    Create : CodeDescription and AlertType to get info about a Result return type or exceptions

---
+++ Src/Shared/Shared.Files/Constants/AlertType.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Files.Constants;
+public record AlertType(string Name) {
+    public static AlertType Error => new(nameof(Error));
+    public static AlertType Warning => new(nameof(Warning));
+    public static AlertType Success => new(nameof(Success));
+}
---
+++ Src/Shared/Shared.Files/Models/MessageDescription.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Files.Constants;
+
+namespace Shared.Files.Models;
+public record MessageDescription(string Code , string Message , AlertType AlertType) {
+    public static MessageDescription New(string code , string message , AlertType alertType)
+        => new(code , message , alertType);
+
+    public static MessageDescription Error(string code , string message)
+       => new(code , message , AlertType.Error);
+
+    public static MessageDescription Success(string code , string message)
+       => new(code , message , AlertType.Success);
+
+    public static MessageDescription Warning(string code , string message)
+      => new(code , message , AlertType.Warning);
+}
