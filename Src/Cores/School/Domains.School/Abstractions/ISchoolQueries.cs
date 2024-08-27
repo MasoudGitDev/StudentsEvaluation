bbca065a162504cb098a77db51b09d7099cd4ffc
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 11:03:09 +0330

    update : add Course,Teachers,Exam Qureies to ISchoolQueries inteface

--- Src/Cores/School/Domains.School/Abstractions/ISchoolQueries.cs
+++ Src/Cores/School/Domains.School/Abstractions/ISchoolQueries.cs
@@ -1,6 +1,12 @@
-﻿using Domains.School.Student.Repo;
+﻿using Domains.School.Course.Repo;
+using Domains.School.ExamResult.Repo;
+using Domains.School.Student.Repo;
+using Domains.School.Teacher.Repo;
 
 namespace Domains.School.Abstractions;
 public interface ISchoolQueries {
     public IStudentQueries Students { get; }
+    public ICourseQueries Courses { get; }
+    public ITeacherQueries Teachers { get; }
+    public IExamResultQueries Exams { get; }
 }
--- Src/Infra/Infra.SqlServerWithEF/Impls/School/SchoolQueries.cs
+++ Src/Infra/Infra.SqlServerWithEF/Impls/School/SchoolQueries.cs
@@ -1,9 +1,21 @@
 ﻿using Domains.School.Abstractions;
+using Domains.School.Course.Repo;
+using Domains.School.ExamResult.Repo;
 using Domains.School.Student.Repo;
+using Domains.School.Teacher.Repo;
 
 namespace Infra.SqlServerWithEF.Impls.School;
 internal sealed class SchoolQueries(
-    IStudentQueries _studentQueries
+    IStudentQueries _studentQueries ,
+    ICourseQueries _courseQueries ,
+    ITeacherQueries _teacherQueries ,
+    IExamResultQueries _examQueries
     ) : ISchoolQueries {
     public IStudentQueries Students => _studentQueries;
+
+    public ICourseQueries Courses => _courseQueries;
+
+    public ITeacherQueries Teachers => _teacherQueries;
+
+    public IExamResultQueries Exams => _examQueries;
 }
