e9017d4175f20e0b3f4fceb1268a1c02f711ab09
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 06:55:08 +0330

    Create : Basic Props for some Table Models

---
+++ Src/Cores/School/Domains.School/Shared/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Domains.School.Shared;
+public class Person {
+    public ulong Id { get; protected set; }
+    public string FirstName { get; protected set; } = null!;
+    public string LastName { get; protected set; } = null!;
+    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    public DateTime? ModifiedAt { get; protected set; }
+
+}
