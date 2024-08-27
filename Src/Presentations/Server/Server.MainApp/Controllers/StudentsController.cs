fa75674931c9c36bb34350f386f82859dc68eabd
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 10:16:21 +0330

    create : basic acions for students

---
+++ Src/Presentations/Server/Server.MainApp/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using Apps.School.Domains.Students.Commands;
+using Apps.School.Domains.Students.Queries;
+using Domains.School.Student.Aggregate;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Files.DTOs;
+using Shared.Files.Models;
+
+namespace Server.MainApp.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController(IMediator _mediator) : ControllerBase {
+
+
+    [HttpGet("All")]
+    public async Task<Result<List<Student>>> GetAllAsync() {
+        return await _mediator.Send(GetStudents.New());
+    }
+
+    [HttpPost("Create")]
+    public async Task<Result> CreateAsync([FromBody] StudentDto model) {
+        return await _mediator.Send(model.Adapt<Create>());
+    }
+
+
+
+}
