5e707e9f318207d5cb9e4bf67b11ddcd7e96d5e7
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 10:15:16 +0330

    Create : Student Dto to handel model in api controller

---
+++ Src/Shared/Shared.Files/DTOs/StudentDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Shared.Files.DTOs;
+public record StudentDto(string FirstName , string LastName , string NationalCode) {
+    public static StudentDto New(string firstName , string lastName , string nationalCode)
+        => new(firstName , lastName , nationalCode);
+}
