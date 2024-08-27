872b1f3b0722b1148833ebd312352aff63de7fe8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 07:41:02 +0330

    create : ConfigurationExtensions and ServiceCollectionExtensions

---
+++ Src/Infra/Infra.SqlServerWithEF/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Infra.SqlServerWithEF.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.SqlServerWithEF.Extensions;
+public static class ServiceCollectionExtensions {
+    public static IServiceCollection AddInfraLayerServices(this IServiceCollection services) {
+
+        var configuration= services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+
+        services.AddDbContext<AppDbContext>(builder => {
+            builder.UseSqlServer(configuration.GetDefaultConnectionString());
+            builder.EnableSensitiveDataLogging();  
+        });
+
+        return services;
+    }
+}
