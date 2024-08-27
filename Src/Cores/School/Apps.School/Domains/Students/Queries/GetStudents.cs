c04a6b21b7d7c2066a09826418699df6682d7c9c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 08:44:04 +0330

    Create :  GetStudents Qurey and its mediatR handler

---
+++ Src/Cores/School/Apps.School/Domains/Students/Queries/GetStudents.cs
@@ -0,0 +1,21 @@
+﻿using Domains.School.Student.Aggregate;
+using Shared.Files.Models;
+using MediatR;
+using Domains.School.Abstractions;
+
+namespace Apps.School.Domains.Students.Queries;
+public sealed record GetStudents : IRequest<Result<List<Student>>> {
+    public static GetStudents New() => new();
+}
+
+
+//================handler
+internal sealed class GetStudentsHandler(ISchoolUOW _unitOfWork) : IRequestHandler<GetStudents , Result<List<Student>>> {
+    public async Task<Result<List<Student>>> Handle(GetStudents request , CancellationToken cancellationToken) {
+        var students = await _unitOfWork.Queries.Students.GetAllAsync();
+        string message = students.Count > 0
+            ? $"{students.Count} students found."
+            : "No students found.";
+        return Result<List<Student>>.Success(message , students);
+    }
+}
