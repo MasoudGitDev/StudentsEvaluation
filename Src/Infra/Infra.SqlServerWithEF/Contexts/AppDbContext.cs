07be877e3ed24a36563ef4aadf3cb860f538c089
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 07:40:26 +0330

    Create : AppDbContext

---
+++ Src/Infra/Infra.SqlServerWithEF/Contexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domains.School.Course.Aggregate;
+using Domains.School.ExamResult.Aggregate;
+using Domains.School.Student.Aggregate;
+using Domains.School.Teacher.Aggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.SqlServerWithEF.Contexts;
+internal class AppDbContext : DbContext {
+
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options) {
+        
+    }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<ExamResult> ExamResults { get; set; }
+}
