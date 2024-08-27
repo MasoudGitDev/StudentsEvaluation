9db24b5a8c008dfdb14ae8932cba9d21ff72f31f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 08:05:55 +0330

    create : Result model as return type of actions

---
+++ Src/Shared/Shared.Files/Models/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace Shared.Files.Models;
+
+public record class Result(bool IsSuccessful , List<MessageDescription> Messages) {
+    public Result() : this(false , [MessageDescription.Warning("Init" , "Parameterless constructor")]) {
+
+    }
+    public static Result Success(string message)
+        => new(true , [MessageDescription.Success("Success" , message)]);
+    public static Result Failed(string message)
+       => new(false , [MessageDescription.Error("Failure" , message)]);
+    public static Result Failed(List<MessageDescription> errors)
+       => new(false , errors);
+}
+
+//=========== generic<T>
+public record class Result<TModel>(bool IsSuccessful , List<MessageDescription> Messages , TModel? Model) {
+    public Result()
+        : this(false , [MessageDescription.Warning("Init" , "Parameterless constructor")] , default) { }
+    public static Result<TModel> Success(string message , TModel model)
+        => new(true , [MessageDescription.Success("Success" , message)] , model);
+    public static Result<TModel> Failed(string message , TModel? model = default)
+         => new(false , [MessageDescription.Error("Failure" , message)] , model);
+    public static Result<TModel> Failed(List<MessageDescription> errors , TModel? model = default)
+      => new(false , errors , model);
+}
