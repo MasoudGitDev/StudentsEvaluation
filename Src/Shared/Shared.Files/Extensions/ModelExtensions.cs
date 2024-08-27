dab63a91b81b3cb1943e8b765ea14e2b22d7c73b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 10:18:27 +0330

    create : ModelExtensions to manage models better

---
+++ Src/Shared/Shared.Files/Extensions/ModelExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Shared.Files.Exceptions;
+using Shared.Files.ValueObjects;
+
+namespace Shared.Files.Extensions {
+    public static class ModelExtensions {
+        public static TModel ThrowIfNull<TModel>(TModel? model , string propertyName) {
+            if(model is null) {
+                throw new CustomException("Null-Object" , $"The {propertyName} object is null.");
+            }
+            return model;
+        }
+        public static TModel? ThrowIfNotNull<TModel>(TModel? model , string propertyName) {
+            if(model is not null) {
+                throw new CustomException("HasValue" , $"The {propertyName} object must be null.");
+            }
+            return model;
+        }
+
+        public static TModel ThrowIfNull<TModel>(TModel? model , Description description) {
+            if(model is null) {
+                throw new CustomException("Null-Object" , description);
+            }
+            return model;
+        }
+        public static TModel? ThrowIfNotNull<TModel>(TModel? model , Description description) {
+            if(model is not null) {
+                throw new CustomException("HasValue" , description);
+            }
+            return model;
+        }
+    }
+}
