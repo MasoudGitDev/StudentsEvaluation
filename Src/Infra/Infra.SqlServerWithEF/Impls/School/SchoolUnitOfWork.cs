73ca0003edcc01bb1ca02296e797564db95c3d2b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-27 10:14:26 +0330

    create : SchoolUnitOfWork

---
+++ Src/Infra/Infra.SqlServerWithEF/Impls/School/SchoolUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Domains.School.Abstractions;
+using Infra.SqlServerWithEF.Contexts;
+
+namespace Infra.SqlServerWithEF.Impls.School;
+
+internal sealed class SchoolUnitOfWork(ISchoolQueries _queries , AppDbContext _dbContext) : ISchoolUOW {
+    public ISchoolQueries Queries => _queries;
+
+    public async Task CreateAsync<TEntity>(TEntity entity) where TEntity : class, new() {
+        await _dbContext.AddAsync(entity , new CancellationToken());
+    }
+
+    public Task DeleteAsync<TEntity>(TEntity entity) where TEntity: class , new(){
+        _dbContext.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync() {
+        await _dbContext.SaveChangesAsync();
+    }
+}
