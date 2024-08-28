e8519f2139225d0942630033ffa745f5117e2c13
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 20:20:50 +0330

    update : remove unused factory method

--- Src/Shared/Shared.Files/DTOs/TeacherDto.cs
+++ Src/Shared/Shared.Files/DTOs/TeacherDto.cs
@@ -1,6 +1,3 @@
 ﻿namespace Shared.Files.DTOs;
 
-public record TeacherDto(string FirstName , string LastName , string PersonnelCode) {
-    public static TeacherDto New(string firstName , string lastName , string personnelCode)
-        => new(firstName , lastName , personnelCode);
-}
+public record TeacherDto(string FirstName , string LastName , string PersonnelCode);
