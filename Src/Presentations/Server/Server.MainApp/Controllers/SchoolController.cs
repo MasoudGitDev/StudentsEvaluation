f01142d088df72f9a6b271bb4bde6dfc5f46999a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 12:08:04 +0330

    update : add ValidateModel Method and ValidationResult to handle dto param with fluent validation and mediaR

--- Src/Presentations/Server/Server.MainApp/Controllers/SchoolController.cs
+++ Src/Presentations/Server/Server.MainApp/Controllers/SchoolController.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentValidation;
+using Mapster;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Server.MainApp.Attributes;
+using Shared.Files.Extensions;
+using Shared.Files.Models;
+using Shared.Files.Validators.Extensions;
 
 namespace Server.MainApp.Controllers;
 
 [ResultException]
 [ApiController]
-public class SchoolController : ControllerBase {
+public class SchoolController(IMediator _mediator , IServiceProvider _serviceProvider) : ControllerBase {
 
+    protected static Result ValidateModel<TValidator, TModel>(TValidator validator , TModel model)
+       where TValidator : AbstractValidator<TModel> {
+        var validation = validator.Validate(model);
+        if(!validation.IsValid) {
+            return Result.Failed(validation.Errors.AsMessageDescriptions());
+        }
+        return Result.Success("The Model is valid");
+    }
+
+    protected async Task<Result> ValidationResult<TValidator, TModel, TDestinationModel>(TModel model)
+        where TValidator : AbstractValidator<TModel>
+        where TDestinationModel : IRequest<Result> {
+        var validator = (_serviceProvider.GetRequiredService<TValidator>()).ThrowIfNull(typeof(TValidator).Name);
+        ValidateModel(validator, model).ThrowIfFailure();
+        return await _mediator.Send(model.Adapt<TDestinationModel>());
+    }
 }
