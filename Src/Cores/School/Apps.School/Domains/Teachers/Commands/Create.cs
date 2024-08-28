0a13fd4bcd736f204df9d59f0caa3615a7cb47dd
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 19:15:32 +0330

    refactor : use SchoolRequestHandler bastarct class and use CreatAndSaveAsync()

--- Src/Cores/School/Apps.School/Domains/Teachers/Commands/Create.cs
+++ Src/Cores/School/Apps.School/Domains/Teachers/Commands/Create.cs
@@ -1,24 +1,23 @@
 ﻿using Domains.School.Abstractions;
+using Domains.School.Shared.Extensions;
 using Domains.School.Teacher.Aggregate;
 using Mapster;
 using MediatR;
+using Shared.Files.Extensions;
 using Shared.Files.Models;
 
 namespace Apps.School.Domains.Teachers.Commands;
-public sealed record Create(string FirstName , string LastName , string PersonnelCode) : IRequest<Result> {
-    public static Create New(string firstName , string lastName , string personnelCode)
-        => new(firstName , lastName , personnelCode);
-}
+public sealed record Create(string FirstName , string LastName , string PersonnelCode) : IRequest<Result>;
 
 //================ handler
-internal sealed class CreateTeacherHandler(ISchoolUOW _unitOfWork) : IRequestHandler<Create , Result> {
-    public async Task<Result> Handle(Create request , CancellationToken cancellationToken) {
-        var findTeacher = (await _unitOfWork.Queries.Teachers.GetByPersonnelCodeAsync(request.PersonnelCode));
-        if(findTeacher is not null) {
-            return Result.Failed($"The teacher with personnel code : <{request.PersonnelCode}> found.");
-        }
-        await _unitOfWork.CreateAsync(request.Adapt<Teacher>());
-        await _unitOfWork.SaveChangesAsync();
-        return Result.Success($"The new teacher with personnel code : <{request.PersonnelCode}> has been created successfully");
+internal sealed class CreateTeacherHandler(ISchoolUOW _unitOfWork)
+    : SchoolRequestHandler<Create , Result>(_unitOfWork.MustHasValue()) {
+    public override async Task<Result> Handle(Create request , CancellationToken cancellationToken) {
+
+        ( await FindTeacherByCodeAsync(request.PersonnelCode) )
+            .ThrowIfNotNull(description: $"A teacher with personnel code : <{request.PersonnelCode}> found.");
+
+        return await CreateAndSaveAsync(request.Adapt<Teacher>() ,
+            $"The new teacher with personnel code : <{request.PersonnelCode}> has been created successfully");
     }
 }
