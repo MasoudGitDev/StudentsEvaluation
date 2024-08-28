ad2c0ef3f4ca553b11aac37767e2e21f034b49ad
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 09:37:45 +0330

    create : GetStudentMeanScore query and its mediatR handler

---
+++ Src/Cores/School/Apps.School/Domains/Students/Queries/GetStudentMeanScore.cs
@@ -0,0 +1,24 @@
+﻿using Domains.School.Abstractions;
+using MediatR;
+using Shared.Files.DTOs;
+using Shared.Files.Extensions;
+using Shared.Files.Models;
+
+namespace Apps.School.Domains.Students.Queries;
+public sealed record GetStudentMeanScore(string NationalCode) : IRequest<Result<StudentMeanScoreDto>> {
+}
+
+//====================handler
+internal sealed class GetStudentMeanScoreHandler(ISchoolUOW _unitOfWork)
+    : SchoolRequestHandler<GetStudentMeanScore , Result<StudentMeanScoreDto>>(_unitOfWork) {
+    public override async Task<Result<StudentMeanScoreDto>> Handle(GetStudentMeanScore request , CancellationToken cancellationToken) {
+        var student = ( await GetStudentByCodeAsync(request.NationalCode) )
+            .ThrowIfNull(description: $"The Student with national code : <{request.NationalCode}> not found.");
+
+        var averageScore = (await GetStudentExamsAsync(student.Id)).Average(x=> x.Score);
+
+        return Result<StudentMeanScoreDto>.Success("Ok" ,
+            new StudentMeanScoreDto(student.FirstName , student.LastName , student.NationalCode , averageScore));
+
+    }
+}
