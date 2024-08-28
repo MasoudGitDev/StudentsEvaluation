b38af15c13215638ce892747dbae4774c1af5592
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 14:32:32 +0330

    refactor : use SchoolRequestHandler and CreateAndSaveAsync

--- Src/Cores/School/Apps.School/Domains/Students/Commands/Create.cs
+++ Src/Cores/School/Apps.School/Domains/Students/Commands/Create.cs
@@ -1,24 +1,23 @@
 ﻿using Domains.School.Abstractions;
+using Domains.School.Shared.Extensions;
 using Domains.School.Student.Aggregate;
-using MediatR;
-using Shared.Files.Models;
 using Mapster;
+using MediatR;
+using Shared.Files.Extensions;
+using Shared.Files.Models;
 
 namespace Apps.School.Domains.Students.Commands;
-public sealed record Create(string FirstName , string LastName , string NationalCode) : IRequest<Result> {
-    public static Create New(string firstName , string lastName , string nationalCode)
-        => new(firstName , lastName , nationalCode);
-}
+public sealed record Create(string FirstName , string LastName , string NationalCode) : IRequest<Result>;
 
 //================ handler
-internal sealed class CreateStudentHandler(ISchoolUOW _unitOfWork) : IRequestHandler<Create , Result> {
-    public async Task<Result> Handle(Create request , CancellationToken cancellationToken) {
-        var findStudent = (await _unitOfWork.Queries.Students.GetByNationalCode(request.NationalCode));
-        if(findStudent is not null) {
-            return Result.Failed($"The student with national code : <{request.NationalCode}> found.");
-        }
-        await _unitOfWork.CreateAsync(request.Adapt<Student>());
-        await _unitOfWork.SaveChangesAsync();
-        return Result.Success($"The new student with national code : <{request.NationalCode}> has been created successfully");
+internal sealed class CreateStudentHandler(ISchoolUOW _unitOfWork)
+    : SchoolRequestHandler<Create , Result>(_unitOfWork.MustHasValue()) {
+    public override async Task<Result> Handle(Create request , CancellationToken cancellationToken) {
+
+        ( await GetStudentByCodeAsync(request.NationalCode) )
+            .ThrowIfNotNull(description: $"The student with national code : <{request.NationalCode}> found.");
+
+        return await CreateAndSaveAsync(request.Adapt<Student>() ,
+            $"The new student with national code : <{request.NationalCode}> has been created successfully.");
     }
 }
