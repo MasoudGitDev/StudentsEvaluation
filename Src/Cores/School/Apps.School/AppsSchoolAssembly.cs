7f45a8cc5db0e338b138dda13d916a05673c0034
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 09:40:01 +0330

    update : use InternalsVisibleTo for showing assembly to unit test

--- Src/Cores/School/Apps.School/AppsSchoolAssembly.cs
+++ Src/Cores/School/Apps.School/AppsSchoolAssembly.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
 
+
+[assembly: InternalsVisibleTo("UnTests.Apps.School")]
 namespace Apps.School;
 public class AppsSchoolAssembly {
 }
