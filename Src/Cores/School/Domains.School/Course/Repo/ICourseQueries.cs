cba49dce3b0b6d8135c31db0cd40edcf528aecf5
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 23:17:42 +0330

    update : add PaginationDto to GetAll method

--- Src/Cores/School/Domains.School/Course/Repo/ICourseQueries.cs
+++ Src/Cores/School/Domains.School/Course/Repo/ICourseQueries.cs
@@ -1,9 +1,10 @@
-﻿using CourseModel=Domains.School.Course.Aggregate.Course;
+﻿using Shared.Files.DTOs;
+using CourseModel=Domains.School.Course.Aggregate.Course;
 
 namespace Domains.School.Course.Repo;
 public interface ICourseQueries {
     Task<CourseModel?> GetByIdAsync(ulong id);
     Task<CourseModel?> GetByCodeAsync(string courseCode);
-    Task<List<CourseModel>> GetAllAsync();
+    Task<List<CourseModel>> GetAllAsync(PaginationDto model);
 
 }
--- Src/Infra/Infra.SqlServerWithEF/Impls/School/CourseQueries.cs
+++ Src/Infra/Infra.SqlServerWithEF/Impls/School/CourseQueries.cs
@@ -2,12 +2,17 @@
 using Domains.School.Course.Repo;
 using Infra.SqlServerWithEF.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Shared.Files.DTOs;
 
 namespace Infra.SqlServerWithEF.Impls.School;
 internal class CourseQueries(AppDbContext _dbContext) : ICourseQueries {
-    public Task<List<Course>> GetAllAsync() {
-        return _dbContext.Courses.ToListAsync();
-    }
+    public async Task<List<Course>> GetAllAsync(PaginationDto model)
+        => model.UsePagination ?
+            await _dbContext.Courses.Skip(( model.PageNumber - 1 ) * model.PageSize)
+                .Take(model.PageSize)
+                .ToListAsync() :
+            await _dbContext.Courses.ToListAsync();
+
 
     public async Task<Course?> GetByCodeAsync(string courseCode) {
         return await _dbContext.Courses.FirstOrDefaultAsync(x => x.Code == courseCode);
