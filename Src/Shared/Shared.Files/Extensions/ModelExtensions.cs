d091f8332e6f02701562a9fa5c62c534dec780fb
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 21:10:08 +0330

    update : all from scratch

--- Src/Shared/Shared.Files/Extensions/ModelExtensions.cs
+++ Src/Shared/Shared.Files/Extensions/ModelExtensions.cs
@@ -1,30 +1,24 @@
 ﻿using Shared.Files.Exceptions;
-using Shared.Files.ValueObjects;
 
 namespace Shared.Files.Extensions;
 public static class ModelExtensions {
+    public static TModel ThrowIfNull<TModel>(this TModel? model , string messageFormat , params string[] names) {
+        if(model is null) {
+            throw new CustomException("Null-Object" , string.Format(messageFormat , names));
+        }
+        return model;
+    }
+
     public static TModel ThrowIfNull<TModel>(this TModel? model , string propertyName) {
         if(model is null) {
             throw new CustomException("Null-Object" , $"The {propertyName} object is null.");
         }
         return model;
     }
-    public static TModel? ThrowIfNotNull<TModel>(this TModel? model , string propertyName) {
-        if(model is not null) {
-            throw new CustomException("HasValue" , $"The {propertyName} object must be null.");
-        }
-        return model;
-    }
 
-    public static TModel ThrowIfNull<TModel>(this TModel? model , Description description) {
-        if(model is null) {
-            throw new CustomException("Null-Object" , description);
-        }
-        return model;
-    }
-    public static TModel? ThrowIfNotNull<TModel>(this TModel? model , Description description) {
+    public static TModel? ThrowIfNotNull<TModel>(this TModel? model , string messageFormat , params string[] names) {
         if(model is not null) {
-            throw new CustomException("HasValue" , description);
+            throw new CustomException("HasValue" , string.Format(messageFormat , names));
         }
         return model;
     }
