2a8bd47e3362f36f4d4eda14a1b1393e2a4374dd
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 14:08:53 +0330

    create : AggregateExtensions to control aggregates  and interfaces better

---
+++ Src/Cores/School/Domains.School/Shared/Extensions/AggregateExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Domains.School.Abstractions;
+using Shared.Files.Exceptions;
+
+namespace Domains.School.Shared.Extensions;
+public static class AggregateExtensions {
+    public static ISchoolUOW MustHasValue(this ISchoolUOW? unitOfWork) {
+        if(unitOfWork is null){
+            throw new CustomException("Null-Object" , "The School unit of work must has value.");
+        }
+        return unitOfWork;
+    }
+}
