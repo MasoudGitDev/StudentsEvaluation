5602ab21345bf3bd904b5d4700a67336561a2941
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 23:15:36 +0330

    update : use PaginationDto as param and change return type (use CourseDto instead Course)

--- Src/Cores/School/Apps.School/Domains/Courses/Queries/GetCourses.cs
+++ Src/Cores/School/Apps.School/Domains/Courses/Queries/GetCourses.cs
@@ -1,18 +1,18 @@
 ﻿using Apps.School.Constants;
 using Domains.School.Abstractions;
-using Domains.School.Course.Aggregate;
 using MediatR;
+using Shared.Files.DTOs;
 using Shared.Files.Models;
 
 namespace Apps.School.Domains.Courses.Queries;
-public sealed record GetCourses() : IRequest<Result<List<Course>>> {
-    public static GetCourses New() => new();
+public sealed record GetCourses(PaginationDto Model) : IRequest<Result<List<CourseDto>>> {
+    public static GetCourses New(PaginationDto model) => new(model);
 }
 
 //====================handler
 internal sealed class GetCoursesHandler(ISchoolUOW _unitOfWork)
-    : SchoolRequestHandler<GetCourses , Result<List<Course>>>(_unitOfWork) {
-    public override async Task<Result<List<Course>>> Handle(GetCourses request , CancellationToken cancellationToken) {
-        return SuccessListResult(nameof(QueryPropertyNames.Courses) , await GetCoursesAsync());
+    : SchoolRequestHandler<GetCourses , Result<List<CourseDto>>>(_unitOfWork) {
+    public override async Task<Result<List<CourseDto>>> Handle(GetCourses request , CancellationToken cancellationToken) {
+        return SuccessListResult(nameof(QueryPropertyNames.Courses) , await GetCoursesAsync(request.Model));
     }
 }
