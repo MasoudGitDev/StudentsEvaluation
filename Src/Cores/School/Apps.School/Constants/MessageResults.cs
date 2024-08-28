23828b2e17aa504cff801d3d86abaf04ae9ec009
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-28 20:14:00 +0330

    Create : Message Result to handle constant messages

---
+++ Src/Cores/School/Apps.School/Constants/MessageResults.cs
@@ -0,0 +1,6 @@
+﻿namespace Apps.School.Constants {
+   public static class MessageResults {
+        public const string FoundTeacher = "A teacher with personnel code : <{0}> found.";
+        public const string CreateTeacher = "The new teacher with personnel code : <{0}> has been created successfully.";
+   }
+}
