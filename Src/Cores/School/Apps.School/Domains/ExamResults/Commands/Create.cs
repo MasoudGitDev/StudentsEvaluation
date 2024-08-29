8a3d46404dac0ce0c39d2ca312b5e8eeba22a724
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-29 11:16:14 +0330

    update : CreateExamResult Command (change params and write better)

--- Src/Cores/School/Apps.School/Domains/ExamResults/Commands/Create.cs
+++ Src/Cores/School/Apps.School/Domains/ExamResults/Commands/Create.cs
@@ -1,15 +1,15 @@
-﻿using Apps.School.Constants;
-using Domains.School.Abstractions;
+﻿using Domains.School.Abstractions;
 using Domains.School.ExamResult.Aggregate;
 using Mapster;
 using MediatR;
+using Shared.Files.Constants;
 using Shared.Files.Extensions;
 using Shared.Files.Models;
 
 namespace Apps.School.Domains.ExamResults.Commands;
-public sealed record Create(ulong StudentId ,
-    ulong TeacherId ,
-    ulong CourseId ,
+public sealed record Create(string TeacherPersonnelCode ,
+    string CourseCode ,
+    string StudentNationalCode ,
     float Score ,
     DateTime ExamDateTime
     ) : IRequest<Result> {
@@ -19,15 +19,32 @@ public sealed record Create(ulong StudentId ,
 internal sealed class CreateExamResultHandler(ISchoolUOW _unitOfWork)
     : SchoolRequestHandler<Create , Result>(_unitOfWork) {
     public override async Task<Result> Handle(Create request , CancellationToken cancellationToken) {
-        if(request.ExamDateTime >= DateTime.UtcNow) {
-            return Result.Failed(MessageResults.ExamDateError);
-        }
-        if(request.Score is < 0 or > 20) {
-            return Result.Failed(String.Format(MessageResults.InvalidScore,request.Score));
-        }
-        ( await HadStudentAnyExamAsync(request.StudentId , request.CourseId , request.ExamDateTime) )
-             .ThrowIfNotNull(MessageResults.OneExamPerCoursePerDay , string.Empty);
+        var (personnelCode, courseCode, nationalCode, score, examDateTime) = request;
+
+        examDateTime.MustDateTimeLessThanNow();
+        score.MustScoreBeInRange();
+
+        await CheckObjectsExistenceAsync(personnelCode , courseCode , nationalCode , examDateTime);
 
         return await CreateAndSaveAsync(request.Adapt<ExamResult>() , MessageResults.CreateExamResult);
     }
+
+    private async Task CheckObjectsExistenceAsync(string teacherPersonnelCode ,
+        string courseCode ,
+        string studentNationalCode ,
+        DateTime examDateTime) {
+
+        ( await FindTeacherByCodeAsync(teacherPersonnelCode) )
+            .ThrowIfNull(MessageResults.NotFoundTeacher , teacherPersonnelCode);
+
+        var course = ( await FindCourseByCodeAsync(courseCode) )
+            .ThrowIfNull(MessageResults.NotFoundCourse , courseCode);
+
+        var student = ( await FindStudentByCodeAsync(studentNationalCode) )
+            .ThrowIfNull(MessageResults.NotFoundStudent , studentNationalCode);
+
+        ( await HadStudentAnyExamAsync(student.Id , course.Id , examDateTime) )
+            .ThrowIfNotNull(MessageResults.OneExamPerCoursePerDay , string.Empty);
+    }
+
 }
