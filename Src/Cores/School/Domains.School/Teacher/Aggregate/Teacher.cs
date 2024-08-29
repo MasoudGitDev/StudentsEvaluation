c764d2dcfc7b997aa986649f07fced0302e3d257
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-29 18:44:31 +0330

    refactor : Teacher aggregate : remove unused namespace

--- Src/Cores/School/Domains.School/Teacher/Aggregate/Teacher.cs
+++ Src/Cores/School/Domains.School/Teacher/Aggregate/Teacher.cs
@@ -1,11 +1,10 @@
 ﻿using Domains.School.Shared;
 using CourseModel = Domains.School.Course.Aggregate.Course;
-using ExamResultModel = Domains.School.ExamResult.Aggregate.ExamResult;
 
 namespace Domains.School.Teacher.Aggregate;
 public partial class Teacher : Person {
     public string PersonnelCode { get; private set; } = null!;
 
     //============relationships
-    public ICollection<CourseModel> Courses { get; private set; }
+    public ICollection<CourseModel> Courses { get; private set; } = [];
 }
