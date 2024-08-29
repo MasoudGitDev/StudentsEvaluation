12f81a4dd61f95f68076f0fe604ffa72f0f6c955
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-29 11:14:18 +0330

    refactor : move MessageResults to Shared project

--- Src/Shared/Shared.Files/Constants/MessageResults.cs
+++ Src/Shared/Shared.Files/Constants/MessageResults.cs
@@ -1,4 +1,4 @@
-﻿namespace Apps.School.Constants;  
+﻿namespace Shared.Files.Constants;
 public static class MessageResults {
     // teacher
     public const string FoundTeacher = "A teacher with personnel code : <{0}> found.";
@@ -11,7 +11,7 @@ public static class MessageResults {
     public const string NotFoundStudent = "The student with national code : <{0}> not found.";
 
     // exam
-    public const string ExamDateError = "The exam has not yet taken place.";
+    public const string DateTimeError = "The date and time must be less than now.";
     public const string InvalidScore = "The score must be within the range of 0 to 20. Provided score: {0}.";
     public const string OneExamPerCoursePerDay = "Each student can only take one exam per course per day.";
     public const string CreateExamResult = "The exam result was created successfully.";
@@ -19,4 +19,5 @@ public static class MessageResults {
     // course
     public const string CreateCourse = "The new course with code: <{0}> has been created successfully.";
     public const string FoundCourse = "A course with code: <{0}> already exists.";
+    public const string NotFoundCourse = "The course with code : <{0}> not found.";
 }
