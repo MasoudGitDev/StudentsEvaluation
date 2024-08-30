d165f1f50abc75679bf43b0ed528b39bcb99aeae
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-30 15:48:43 +0330

    update : Teacher : add new version of NEW

--- Src/Cores/School/Domains.School/Teacher/Aggregate/TeacherCommands.cs
+++ Src/Cores/School/Domains.School/Teacher/Aggregate/TeacherCommands.cs
@@ -1,7 +1,14 @@
 ﻿namespace Domains.School.Teacher.Aggregate;
 public partial class Teacher {
-    public static Teacher New(string firstName , string lastName , string personnelCode) 
-        => new() { 
+    public static Teacher New(string firstName , string lastName , string personnelCode)
+        => new() {
+            FirstName = firstName ,
+            LastName = lastName ,
+            PersonnelCode = personnelCode
+        };
+    public static Teacher New(ulong id , string firstName , string lastName , string personnelCode)
+        => new() {
+            Id = id ,
             FirstName = firstName ,
             LastName = lastName ,
             PersonnelCode = personnelCode
