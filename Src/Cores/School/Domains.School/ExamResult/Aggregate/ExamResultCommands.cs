80a56a43e9bdbf22e44c2d99b5041970ae03ccd9
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-30 17:45:34 +0330

    update : ExamResult : add neversion of New() to handle unit testing better

--- Src/Cores/School/Domains.School/ExamResult/Aggregate/ExamResultCommands.cs
+++ Src/Cores/School/Domains.School/ExamResult/Aggregate/ExamResultCommands.cs
@@ -1,4 +1,6 @@
-﻿namespace Domains.School.ExamResult.Aggregate;
+﻿using CourseModel = Domains.School.Course.Aggregate.Course;
+
+namespace Domains.School.ExamResult.Aggregate;
 public partial class ExamResult {
     public static ExamResult New(
         ulong courseId ,
@@ -13,4 +15,22 @@ public partial class ExamResult {
             ExamDateTime = examDate ,
             Score = score
         };
+
+
+    // for unit testing
+    public static ExamResult New( ulong id,
+       CourseModel course ,
+       ulong teacherId ,
+       ulong studentId ,
+       DateTime examDate ,
+       float score)
+       => new() {
+           Id = id ,
+           CourseId = course.Id ,
+           TeacherId = teacherId ,
+           StudentId = studentId ,
+           ExamDateTime = examDate ,
+           Score = score ,
+           Course = course
+       };
 }
