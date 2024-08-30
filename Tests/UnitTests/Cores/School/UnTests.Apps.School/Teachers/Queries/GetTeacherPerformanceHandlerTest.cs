9596f35e1029a39e5733b6ed154f5168a4409ef2
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-30 17:48:58 +0330

    update : GetTeacherPerformanceHandlerTest : improve by using exams insead of student

--- Tests/UnitTests/Cores/School/UnTests.Apps.School/Teachers/Queries/GetTeacherPerformanceHandlerTest.cs
+++ Tests/UnitTests/Cores/School/UnTests.Apps.School/Teachers/Queries/GetTeacherPerformanceHandlerTest.cs
@@ -3,14 +3,13 @@ using Domains.School.Abstractions;
 using Domains.School.Course.Aggregate;
 using Domains.School.ExamResult.Aggregate;
 using Domains.School.Student.Aggregate;
-using Domains.School.StudentCourse.Aggregate;
 using Domains.School.Teacher.Aggregate;
 using FluentAssertions;
 using Moq;
 using Shared.Files.DTOs;
 using Shared.Files.Models;
 
-namespace UnTests.Apps.School.Teachers.Queries;  
+namespace UnTests.Apps.School.Teachers.Queries;
 public class GetTeacherPerformanceHandlerTest {
 
     private readonly Mock<ISchoolUOW> _mockUnitOfWork = new();
@@ -25,9 +24,13 @@ public class GetTeacherPerformanceHandlerTest {
         //Arrange
         GetTeacherPerformance request = new("Teacher_PC_1");
 
-        Teacher teacher = Teacher.New("Teacher_FN_1" , "Teacher_LN_1" , request.PersonnelCode);
-        teacher.Courses = CreateTeacherCourses(teacher.Id , 3);
-        _mockUnitOfWork.Setup(x=> x.Queries.Teachers.GetByPersonnelCodeAsync(teacher.PersonnelCode))
+        Teacher teacher = Teacher.New(1,"Teacher_FN_1" , "Teacher_LN_1" , request.PersonnelCode);
+        teacher.Courses = CreateTeacherCourses(teacher ,
+            numberOfCourses: 1 ,
+            numberOfStudents : 3 ,
+            numberOfExams : 1);
+
+        _mockUnitOfWork.Setup(x => x.Queries.Teachers.GetByPersonnelCodeAsync(teacher.PersonnelCode))
             .ReturnsAsync(teacher);
 
         //Act
@@ -35,7 +38,7 @@ public class GetTeacherPerformanceHandlerTest {
 
         //Act
         result.Should().NotBeNull().And
-            .BeOfType<Result< List<TeacherPerformanceDto>>>();
+            .BeOfType<Result<List<TeacherPerformanceDto>>>();
         _mockUnitOfWork.VerifyAll();
     }
 
@@ -44,34 +47,43 @@ public class GetTeacherPerformanceHandlerTest {
 
 
     //==================private
-    private static ICollection<Course> CreateTeacherCourses(ulong teacherId , int numberOfCourses = 10 , int numberOfStudents = 10) {
+    private static ICollection<Course> CreateTeacherCourses(Teacher teacher,
+        int numberOfCourses = 3 ,
+        int numberOfStudents = 10 , 
+        int numberOfExams = 10) {
         ICollection<Course> courses = [];
         for(int i = 1 ; i <= numberOfCourses ; i++) {
-            var course = Course.New("Course_Code_"+i , "Course_Name_"+i , teacherId);
-            course.Students = CreateStudentsCourses(teacherId, numberOfStudents);
+            var course = Course.New((ulong)i ,"Course_Code_"+i , "Course_Name_"+i , teacher.Id);
+            course.Exams = CreateExamResults(teacher.Id , course , numberOfStudents , numberOfExams);
             courses.Add(course);
         }
         return courses;
     }
-    private static ICollection<StudentCourse> CreateStudentsCourses(ulong teacherId,int studentCourseNumber = 10) {
-        ICollection<StudentCourse> studentsCourses = [];
-        for(int i = 1 ; i <= studentCourseNumber ; i++) {
-            var course = Course.New("Course_Code_"+i, "Course_Name_"+i ,teacherId );
-            var student = Student.New("Student_FN_"+i,"Student_LN_"+i , "Student_NC_"+i);
-            student.Exams = CreateStudentExamResults(course.Id , teacherId , student.Id);
-            StudentCourse studentCourse = new(){
-                Student = student ,
-                Course = course
-            };
-            studentsCourses.Add(studentCourse);
+
+
+    private static ICollection<ExamResult> CreateExamResults(ulong teacherId ,
+        Course course ,
+        int studentNumber = 10 ,
+        int examNumber = 10) {
+        List<ExamResult> studentsExams = [];
+        for(int i = 1 ; i <= studentNumber ; i++) {
+            var student = Student.New((ulong)i,"Student_FN_"+i,"Student_LN_"+i , "Student_NC_"+i);
+            var studentExams = CreateStudentExamResults(course , teacherId , student.Id,examNumber);
+            student.Exams = studentExams;
+            studentsExams.AddRange(studentExams);
         }
-        return studentsCourses;
+        return studentsExams;
     }
 
-    private static List<ExamResult> CreateStudentExamResults(ulong courseId , ulong teacherId , ulong studentId , int examNumber = 10) {
+    private static List<ExamResult> CreateStudentExamResults(Course course , ulong teacherId , ulong studentId , int examNumber = 10) {
         List<ExamResult> examResults = [];
         for(int i = 1 ; i <= examNumber ; i++) {
-            examResults.Add(ExamResult.New(courseId , teacherId , studentId , DateTime.Today.AddDays(-i) , new Random().Next(14,20)));
+            examResults.Add(ExamResult.New((ulong)i ,
+                course , 
+                teacherId , 
+                studentId , 
+                DateTime.Today.AddDays(-i) , 
+                new Random().Next(14 , 20)));
         }
         return examResults;
     }
