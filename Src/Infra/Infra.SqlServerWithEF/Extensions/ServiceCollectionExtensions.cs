b1fb88ac998f6daeb0a7e1ff6e3d970a7d6e8ee1
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-30 15:55:17 +0330

    update : Infra services : add dummy data

--- Src/Infra/Infra.SqlServerWithEF/Extensions/ServiceCollectionExtensions.cs
+++ Src/Infra/Infra.SqlServerWithEF/Extensions/ServiceCollectionExtensions.cs
@@ -5,14 +5,16 @@ using Domains.School.Shared.Abstractions;
 using Domains.School.Student.Repo;
 using Domains.School.Teacher.Repo;
 using Infra.SqlServerWithEF.Contexts;
+using Infra.SqlServerWithEF.DummyData;
 using Infra.SqlServerWithEF.Impls.School;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Files.Extensions;
 
 namespace Infra.SqlServerWithEF.Extensions;
 public static class ServiceCollectionExtensions {
-    public static IServiceCollection AddInfraLayerServices(this IServiceCollection services) {
+    public static async Task<IServiceCollection> AddInfraLayerServices(this IServiceCollection services) {
 
         var configuration= services.BuildServiceProvider().GetRequiredService<IConfiguration>();
 
@@ -29,6 +31,11 @@ public static class ServiceCollectionExtensions {
 
         services.AddScoped<ISchoolQueries , SchoolQueries>();
         services.AddScoped<ISchoolUOW , SchoolUnitOfWork>();
+
+        // ====== add dummy data
+        ISchoolUOW unitOfWork = services.BuildServiceProvider().GetRequiredService<ISchoolUOW>();
+        var dummyData = new ProjectDummyData(unitOfWork);
+        await dummyData.ExecuteAsync();
         return services;
     }
 }
