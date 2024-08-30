4992747507a108ef10141a318540c741224b2b9c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-30 22:10:46 +0330

    udpate : Result Model : add Warning method

--- Src/Shared/Shared.Files/Models/Result.cs
+++ Src/Shared/Shared.Files/Models/Result.cs
@@ -18,6 +18,8 @@ public record class Result<TModel>(bool IsSuccessful , List<MessageDescription> 
         : this(false , [MessageDescription.Warning("Init" , "Parameterless constructor")] , default) { }
     public static Result<TModel> Success(string message , TModel model)
         => new(true , [MessageDescription.Success("Success" , message)] , model);
+    public static Result<TModel> Warning(string message , TModel model)
+      => new(true , [MessageDescription.Warning("Warning" , message)] , model);
     public static Result<TModel> Failed(string message , TModel? model = default)
          => new(false , [MessageDescription.Error("Failure" , message)] , model);
     public static Result<TModel> Failed(List<MessageDescription> errors , TModel? model = default)
