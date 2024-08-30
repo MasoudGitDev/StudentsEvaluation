2a7a41b212c47ad14ee91148007205d6e29cf905
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-30 11:24:25 +0330

    update : MessageDescription : remove AlertType param to show messageDescription more readable and simple for now

--- Src/Shared/Shared.Files/Models/MessageDescription.cs
+++ Src/Shared/Shared.Files/Models/MessageDescription.cs
@@ -1,16 +1,26 @@
 ﻿using Shared.Files.Constants;
 
 namespace Shared.Files.Models;
-public record MessageDescription(string Code , string Message , AlertType AlertType) {
-    public static MessageDescription New(string code , string message , AlertType alertType)
-        => new(code , message , alertType);
-
+public record MessageDescription(string Code , string Message) {
     public static MessageDescription Error(string code , string message)
-       => new(code , message , AlertType.Error);
+       => new(code , message);
 
     public static MessageDescription Success(string code , string message)
-       => new(code , message , AlertType.Success);
+       => new(code , message );
 
     public static MessageDescription Warning(string code , string message)
-      => new(code , message , AlertType.Warning);
+      => new(code , message);
 }
+//public record MessageDescription(string Code , string Message , AlertType AlertType) {
+//    public static MessageDescription New(string code , string message , AlertType alertType)
+//        => new(code , message , alertType);
+
+//    public static MessageDescription Error(string code , string message)
+//       => new(code , message , AlertType.Error);
+
+//    public static MessageDescription Success(string code , string message)
+//       => new(code , message , AlertType.Success);
+
+//    public static MessageDescription Warning(string code , string message)
+//      => new(code , message , AlertType.Warning);
+//}
