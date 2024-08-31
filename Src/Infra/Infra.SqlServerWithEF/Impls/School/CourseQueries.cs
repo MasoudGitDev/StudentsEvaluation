65a140a27e48380fdbf9f8ec31e5f014e0254ba2
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-31 14:51:01 +0330

    refactor : CourseQueries: GetAllAsync

--- Src/Infra/Infra.SqlServerWithEF/Impls/School/CourseQueries.cs
+++ Src/Infra/Infra.SqlServerWithEF/Impls/School/CourseQueries.cs
@@ -6,13 +6,16 @@ using Shared.Files.DTOs;
 
 namespace Infra.SqlServerWithEF.Impls.School;
 internal class CourseQueries(AppDbContext _dbContext) : ICourseQueries {
-    public async Task<List<Course>> GetAllAsync(PaginationDto model)
-        => model.UsePagination ?
-            await _dbContext.Courses.Include(x=>x.Teacher).Skip(( model.PageNumber - 1 ) * model.PageSize)
-                .Take(model.PageSize)
-                .ToListAsync() :
-            await _dbContext.Courses.Include(x => x.Teacher).ToListAsync();
-
+    public async Task<List<Course>> GetAllAsync(PaginationDto model) {
+        var courses = _dbContext.Courses.Include(x=>x.Teacher);
+        if(model.UsePagination) {
+            return await courses
+                    .Skip(( model.PageNumber - 1 ) * model.PageSize)
+                    .Take(model.PageSize)
+                    .ToListAsync();
+        }
+        return await courses.ToListAsync();
+    }
 
     public async Task<Course?> GetByCodeAsync(string courseCode) {
         return await _dbContext.Courses.FirstOrDefaultAsync(x => x.Code == courseCode);
