fbfef168c268b717911fbf06108099e1118a88a8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-31 23:54:53 +0330

    update : Program : register : ExamResultDtoValidator

--- Src/Presentations/Server/Server.MainApp/Program.cs
+++ Src/Presentations/Server/Server.MainApp/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddMediatR(config => {
 builder.Services.AddTransient(service => new StudentDtoValidator());
 builder.Services.AddTransient(service => new CourseDtoValidator());
 builder.Services.AddTransient(service => new TeacherDtoValidator());
+builder.Services.AddTransient(service => new ExamResultDtoValidator());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,8 +28,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if(app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
