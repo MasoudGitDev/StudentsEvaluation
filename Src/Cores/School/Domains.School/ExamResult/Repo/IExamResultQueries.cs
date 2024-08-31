9747dead3a53fab1abc28b0519b7e9c3f6aa180b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-31 23:14:13 +0330

    del : ExemResultQueries : remove unused queries

--- Src/Cores/School/Domains.School/ExamResult/Repo/IExamResultQueries.cs
+++
@@ -1,8 +0,0 @@
-﻿using ExamResultModel = Domains.School.ExamResult.Aggregate.ExamResult;
-
-namespace Domains.School.ExamResult.Repo;
-public interface IExamResultQueries {
-    Task<ExamResultModel?> GetByIdAsync(ulong id);
-    Task<List<ExamResultModel>> GetAllAsync();
-    Task<List<ExamResultModel>> GetStudentExamsAsync(ulong studentId);
-}
