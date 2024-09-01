ca4445e8a1309cae97e8c721fb3b82d4a9e2048d
MasoudSh <Msh.Git.Dev@gmail.com>
2024-09-01 14:56:38 +0330

    create : DbConnection : to handle DefaultConnectionString in AppDbContextFactory

---
+++ Src/Infra/Infra.SqlServerWithEF/Contexts/DbConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace Infra.SqlServerWithEF.Contexts;
+
+/// <summary>
+/// Just used in AppDbContextFactory
+/// </summary>
+internal class DbConnection {
+    public static string DefaultConnectionString
+        => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentEvalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+}
