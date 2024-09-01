58d6c3cf9cd1c4934347489fa3b82cdd2ee4838a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-09-01 14:58:10 +0330

    refactor: ConfigurationExtensions : remove unused TempDefaultConnectionString

--- Src/Infra/Infra.SqlServerWithEF/Extensions/ConfigurationExtensions.cs
+++ Src/Infra/Infra.SqlServerWithEF/Extensions/ConfigurationExtensions.cs
@@ -5,8 +5,5 @@ public static class ConfigurationExtensions {
     public static string GetDefaultConnectionString(this IConfiguration configuration) {
         return configuration.GetConnectionString("Default") 
             ?? throw new ArgumentNullException(nameof(configuration));
-    }
-
-    public static string TempDefaultConnectionString 
-        => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentEvalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    }  
 }
